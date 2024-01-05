9426b3bab2073c355f67571aae0d517abd81ff56
Xwaffle1 <chasecool56@gmail.com>
2024-01-05 14:53:29 -0500

    Finished frontend for revamp

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,13 @@
-import '../styles/globals.css'
+"use client";
+import "../styles/globals.css";
+import '../styles/prism-vs.css'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <div>
+      <Component {...pageProps} />
+    </div>
+  );
 }
 
-export default MyApp
+export default MyApp;
