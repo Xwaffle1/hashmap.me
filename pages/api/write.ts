bcff4033e721847b4acb240e2389b67088b6f843
Xwaffle1 <chasecool56@gmail.com>
2024-01-06 17:14:35 -0500

    Fix collection size ts

--- pages/api/write.ts
+++ pages/api/write.ts
@@ -1,5 +1,6 @@
 import nextConnect from 'next-connect';
 import dbClass from '../../util/database';
+import { Db } from 'mongodb';
 
 const cors = require('cors')
 
@@ -15,8 +16,9 @@ handler.options('*', cors(corsOptions)); // preflight OPTIONS;
 
 
 handler.put(async (req, res) => {
-  let db = req.db
-  
+  //@ts-ignore
+  let db : Db = req.db
+  //@ts-ignore
   console.log(req.body)
   res.setHeader('Access-Control-Allow-Origin', '*')
   res.setHeader('Access-Control-Allow-Methods', '*')
@@ -24,6 +26,7 @@ handler.put(async (req, res) => {
 
   if(req.headers['x-api-key'] == undefined){
     // console.log("NO API TOKEN.")
+    //@ts-ignore
     res.status(400).json({message: "Please provide an x-api-key to access this dataset."})
     return
   }
@@ -32,23 +35,27 @@ handler.put(async (req, res) => {
   // Ensure request is is json application content type
   if (req.method === 'PUT' && req.headers['content-type'] != "application/json"){
     // console.log("NOT JSON.")
+    //@ts-ignore
     res.status(400).json({message: "Error. Please provide a JSON body, with the Content-Type header set to application/json."})
     return
   }
-
+//@ts-ignore
   if (req.body.key == undefined && req.body.KEY == ""){
     // console.log("NO KEY.")
+    //@ts-ignore
     res.status(400).json({message: "Error. Please provide a KEY in your JSON request body for the hashmap record you are attempting to put."})
     return
   }
-
+//@ts-ignore
   if (req.body.value == undefined && req.body.VALUE == undefined){
     // console.log("NO VALUE.")
+    //@ts-ignore
     res.status(400).json({message: "Error. Please provide a VALUE in your JSON request body for the hashmap record you are attempting to put."})
     return
   }
-
+//@ts-ignore
   const key = req.body.key ?? req.body.KEY
+  //@ts-ignore
   const value = req.body.value ?? req.body.VALUE
   
   let tokensCollection = await db.collection("hashmapme-tokens")
@@ -56,6 +63,7 @@ handler.put(async (req, res) => {
   
   if(index.length == 0){
     console.error("No collection found with UUID " + accessToken)
+    //@ts-ignore
     res.status(400).json({message: "Error. No collection found with your accessToken. Please check your token, and try again."});
     return
   }
@@ -65,12 +73,14 @@ handler.put(async (req, res) => {
   var collectionName = index[0].collectionName
   var collectionByUUID = await db.collection(collectionName)
 
-  var collectionStats = await collectionByUUID.totalSize()
-  console.log("CollectionSize: " + collectionStats.size)
+  // get the collections stats to check size
+  const collectionStats = await db.command({ collStats: collectionName });
+  console.log("CollectionSize: " + collectionStats.size);
   console.log("collectionName: " + collectionName)
 
   if (collectionStats.size >= 1000 * 1000 * 1000){
       console.log("DATA TOO LARGE")
+      //@ts-ignore
       res.status(400).json({message: "Error. Large collection. Hey cutie, I wasn't expecting us to move this fast. Your collection is greater than 1GB..."})
   }
 
@@ -81,11 +91,13 @@ handler.put(async (req, res) => {
 
   if (currentIndexWithKey.length == 0){
     // console.log("Inserted into Database..");
-    let doc = await collectionByUUID.insertOne({key: key, value: value});        
+    let doc = await collectionByUUID.insertOne({key: key, value: value}); 
+    //@ts-ignore       
     res.status(200).json(doc);
   }else{
     // console.log("UPDATING Database..");
     let doc = await collectionByUUID.updateOne({ key: key}, {$set: {value: value}});        
+    //@ts-ignore
     res.status(200).json(doc);
   }
   
